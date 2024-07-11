79eb3470b794eb3e18d99d8769c3c77d268e6433
Pandora <140202377+Fukkyil@users.noreply.github.com>
2024-07-11 13:16:19 -0300

    Camera WIP
    Adding the camera functionality, currently WIP

--- Camera.cs
+++ Camera.cs
@@ -5,19 +5,97 @@ using System.Net.Http.Headers;
 public partial class Camera : Node3D
 {
     [Export]
-    public double interpolation_speed = 15.0;
+    public bool invert_y;
     [Export]
-    public Node3D target; 
+    public bool invert_x;
     [Export]
-    public Vector3 offset = new Vector3(0,12,-12);
+    public float mouse_sensitivity = 0.005f;
+    [Export]
+    public float max_fov = 90;
+    [Export]
+    public float default_fov = 60;
+    [Export]
+    public float min_fov = 30;
+    [Export]
+    public float fov_multiplier = 1f;
+    [Export]
+    public float fov_larp_speed = 0.05f;
+
+    private Vector3 offset = new Vector3(0, 5, 15);
+    private Node3D innerGimbal;
+    private Camera3D camera;
+    private ShipController shipController;
+    private int dirY;
+    private int dirX;
+
+    public override void _Ready()
+    {
+        innerGimbal = GetNode<Node3D>("InnerGimbal");
+        shipController = GetNode<ShipController>("../Spaceship");
+        camera = GetNode<Camera3D>("InnerGimbal/Camera3D");
+
+        camera.GlobalPosition = offset + GlobalPosition;
+
+        if(shipController != null){
+            GD.Print("Shipcontroller found!");
+            GD.Print(shipController);
+        }
+        else{
+            GD.Print("Shipcontroller not found!");
+        }
+
+        if(camera != null){
+            GD.Print("Camera Found!");
+            GD.Print(camera);
+        }
+        else{
+            GD.Print("Camera not found!");
+        }
+
+        //Mouse Inversion setup
+        //Interesting way of making an if statement I saw in a blog: var x = statement ? true : false;
+        dirY = invert_y ? dirY = 1 : dirY = -1;
+        dirX = invert_x ? dirX = 1 : dirX = -1;
+    }
+
+
+    public override void _UnhandledInput(InputEvent @event)
+{
+    //Camera mouse controls
+        if (@event is InputEventMouseMotion mouseEvent)
+        {
+            if (mouseEvent.Relative.X != 0)
+            {
+                RotateObjectLocal(Vector3.Up, dirX * mouseEvent.Relative.X * mouse_sensitivity);
+            }
+
+            if (mouseEvent.Relative.Y != 0)
+            {
+                innerGimbal.RotateObjectLocal(Vector3.Right, dirY * mouseEvent.Relative.Y * mouse_sensitivity);
+                GD.Print(innerGimbal.Rotation + " Ship: " + shipController.Rotation);
+            }
+
+        }
+    }
 
     public override void _Process(double delta)
     {
-        if (target != null){
-            Transform3D target_xform = target.GlobalTransform.TranslatedLocal(offset);
-            GlobalTransform = GlobalTransform.InterpolateWith(target_xform, (float)interpolation_speed * (float)delta);
 
-            LookAt(target.GlobalTransform.Origin, target.Transform.Basis.Y);
+        //FOV Adjustments
+        if(shipController.isthrustringforward){
+            camera.Fov = Mathf.Lerp(camera.Fov, max_fov * fov_multiplier, (shipController.forward_speed * fov_larp_speed) * (float)delta);
         }
+        else if(shipController.isthrustingbackward){
+            camera.Fov = Mathf.Lerp(camera.Fov, min_fov * fov_multiplier, (shipController.forward_speed * fov_larp_speed) * (float)delta);
+        }
+        else{
+            camera.Fov = Mathf.Lerp(camera.Fov, default_fov * fov_multiplier, (shipController.forward_speed * fov_larp_speed) * (float)delta);
+        }
+        //GD.Print("Fov: " + camera.Fov);
+
+        GlobalPosition = shipController.GlobalPosition;
+
+        
+        
     }
 }
--- ShipController.cs
+++ ShipController.cs
@@ -2,70 +2,80 @@ using Godot;
 
 public partial class ShipController : RigidBody3D
 {
-    [Export]
-    public float max_speed = 50;
-    [Export]
-    public float natural_speed = 25;
-    [Export]
-    public float brake_speed = 5;
-    [Export]
-    public float acceleration = 0.6f;
-    [Export]
-    public float pitch_speed = 1.5f;
-    [Export]
-    public float roll_speed = 1.9f;
-    [Export]
-    public float yaw_speed = 1.25f;
-    private float forward_speed = 30;
+	[Export]
+	public float max_speed = 50;
+	[Export]
+	public float natural_speed = 25;
+	[Export]
+	public float brake_speed = 5;
+	[Export]
+	public float acceleration = 0.6f;
+	[Export]
+	public float pitch_speed = 1.5f;
+	[Export]
+	public float roll_speed = 1.9f;
+	[Export]
+	public float yaw_speed = 1.25f;
+	public float forward_speed = 30;
+    public bool isthrustringforward;
+    public bool isthrustingbackward;
+ 
 
-    private float pitch_input = 0;
-    private float yaw_input = 0;
-    private float roll_input = 0;
-    Vector3 velocity;
+	private float pitch_input = 0;
+	private float yaw_input = 0;
+	private float roll_input = 0;
+	Vector3 velocity;
 
-    public override void _Process(double delta)
-    {
-        if(Input.IsActionPressed("throttle_up")){
-            forward_speed = (float)Mathf.Lerp(forward_speed, max_speed, acceleration * delta);
-        }
-        else if(Input.IsActionPressed("throttle_down")){
-            forward_speed = (float)Mathf.Lerp(forward_speed, brake_speed, acceleration * delta);
-        }
-        else{
-            forward_speed = (float)Mathf.Lerp(forward_speed, natural_speed, acceleration * delta);
-        }
-
-        pitch_input = Input.GetAxis("pitch_down", "pitch_up");
-        yaw_input = Input.GetAxis("yaw_right", "yaw_left");
-        roll_input = Input.GetAxis("roll_right", "roll_left");
-
-        Transform3D currentTransform = GlobalTransform;
-
-       Basis rotatedBasis = currentTransform.Basis;
-       rotatedBasis = rotatedBasis.Rotated(Transform.Basis.Z, roll_input * roll_speed * (float)delta);
-       rotatedBasis = rotatedBasis.Rotated(Transform.Basis.X, pitch_input * pitch_speed * (float)delta);
-       rotatedBasis = rotatedBasis.Rotated(Transform.Basis.Y, yaw_input * yaw_speed * (float)delta);
+	public override void _Process(double delta)
+	{
+		if(Input.IsActionPressed("throttle_up")){
+			forward_speed = (float)Mathf.Lerp(forward_speed, max_speed, acceleration * delta);
+            isthrustringforward = true;
+            isthrustingbackward = false;
+		}
+		else if(Input.IsActionPressed("throttle_down")){
+			forward_speed = (float)Mathf.Lerp(forward_speed, brake_speed, acceleration * delta);
+            isthrustingbackward = true;
+            isthrustringforward = false;
+		}
+		else{
+			forward_speed = (float)Mathf.Lerp(forward_speed, natural_speed, acceleration * delta);
+            isthrustingbackward = false;
+            isthrustringforward = false;
+		}
 
 
-       rotatedBasis = rotatedBasis.Orthonormalized();
+		pitch_input = Input.GetAxis("pitch_down", "pitch_up");
+		yaw_input = Input.GetAxis("yaw_right", "yaw_left");
+		roll_input = Input.GetAxis("roll_right", "roll_left");
 
-       currentTransform.Basis = rotatedBasis;
-       GlobalTransform = currentTransform;
+		Transform3D currentTransform = GlobalTransform;
 
-    }
+	   Basis rotatedBasis = currentTransform.Basis;
+	   rotatedBasis = rotatedBasis.Rotated(Transform.Basis.Z, roll_input * roll_speed * (float)delta);
+	   rotatedBasis = rotatedBasis.Rotated(Transform.Basis.X, pitch_input * pitch_speed * (float)delta);
+	   rotatedBasis = rotatedBasis.Rotated(Transform.Basis.Y, yaw_input * yaw_speed * (float)delta);
 
-    public override void _PhysicsProcess(double delta)
-    {
-       Vector3 forward = -GlobalTransform.Basis.Z;
-       velocity = forward * forward_speed;
-       
-       MoveAndCollide(velocity * (float)delta);
-       
 
-       GD.Print("Position: " + GlobalPosition + " | speed: " + forward_speed);
-       GD.Print("Rotation: " + GlobalRotation);
-       GD.Print("Roll Input: " + roll_input + " Pitch Input: " + pitch_input + " Yaw Input: " + yaw_input);
-       
-    }
+	   rotatedBasis = rotatedBasis.Orthonormalized();
+
+	   currentTransform.Basis = rotatedBasis;
+	   GlobalTransform = currentTransform;
+
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+	   Vector3 forward = -GlobalTransform.Basis.Z;
+	   velocity = forward * forward_speed;
+	   
+	   MoveAndCollide(velocity * (float)delta);
+	   
+
+	   //GD.Print("Position: " + GlobalPosition + " | speed: " + forward_speed);
+	   //GD.Print("Rotation: " + GlobalRotation);
+	   //GD.Print("Roll Input: " + roll_input + " Pitch Input: " + pitch_input + " Yaw Input: " + yaw_input);
+	   
+	}
 
 }
